f51e95c007cf666400ad6b91f3eed06277570a76
RainbowZap223 <79471824+RainbowZap223@users.noreply.github.com>
2021-08-11 18:31:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+    noseX = 0;
+    noseY = 0;
+
+    leftWristX = 0;
+    rightWristX = 0;
+    difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 550);
@@ -16,9 +23,25 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+
+        noseX = results[0].pose.nose.X;
+        noseY = results[0].pose.nose.y;
+
+        console.log("Nose x = " + noseX + " , Nose y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left wrist x = " + leftWristX + " , Right wrist x = " +rightWristX + " , Difference = " + difference);
+        
     }
 }
 
 function draw() {
     background('#4287f5');
+    fill('#fcf403');
+    stroke('#fc8403');
+    square(noseX , noseY, difference);
+
 }
